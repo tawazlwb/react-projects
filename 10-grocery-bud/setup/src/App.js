21279fdde98cf4cf56c93b4c343099a8b37901ba
Ismail KHAIRY <tawazlwb@gmail.com>
2022-10-04 12:14:27 +0200

    130. Grocery Bud - Edit Item

--- 10-grocery-bud/setup/src/App.js
+++ 10-grocery-bud/setup/src/App.js
@@ -17,6 +17,22 @@ function App() {
       showAlert(true, 'danger', 'please enter value')
     } else if (name && isEditing) {
       // deal with edit
+      setList(
+        list.map((item) => {
+          if (item.id === editID) {
+            return { ...item, title: name }
+          }
+
+          return item
+        })
+      )
+
+      setName('')
+      setEditID(null)
+      setIsEditing(false)
+
+      // show alert
+      showAlert(true, 'success', 'value changed')
     } else {
       // show alert
       showAlert(true, 'success', 'item added to the list')
@@ -43,6 +59,13 @@ function App() {
     setList(newList)
   }
 
+  const editItem = (id) => {
+    const specificItem = list.find((item) => item.id === id)
+    setIsEditing(true)
+    setEditID(id)
+    setName(specificItem.title)
+  }
+
   return (
     <section className='section-center'>
       {/* grocery form */}
@@ -66,7 +89,7 @@ function App() {
       {/* List of items */}
       {list.length > 0 && (
         <div className='grocery-container'>
-          <List items={list} removeItem={removeItem} />
+          <List items={list} removeItem={removeItem} editItem={editItem} />
           <button className='clear-btn' onClick={clearList}>
             clean items
           </button>
--- 10-grocery-bud/setup/src/List.js
+++ 10-grocery-bud/setup/src/List.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { FaEdit, FaTrash } from 'react-icons/fa'
 
-const List = ({ items, removeItem }) => {
+const List = ({ items, removeItem, editItem }) => {
   return (
     <div className='grocery-list'>
       {items.map((item) => {
@@ -11,7 +11,7 @@ const List = ({ items, removeItem }) => {
             <p className='title'>{title}</p>
             <div className='btn-container'>
               <button type='button' className='edit-btn'>
-                <FaEdit />
+                <FaEdit onClick={() => editItem(id)} />
               </button>
               <button type='button' className='delete-btn'>
                 <FaTrash onClick={() => removeItem(id)} />
