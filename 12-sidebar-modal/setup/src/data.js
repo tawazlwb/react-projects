306018ba94369d7a51366ca35e3847e23c4e5e67
Ismail KHAIRY <tawazlwb@gmail.com>
2022-10-04 17:58:59 +0200

    140. Sidebar/Modal - Structure

--- 12-sidebar-modal/setup/src/data.js
+++ 12-sidebar-modal/setup/src/data.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from 'react'
 import {
   FaBehance,
   FaFacebook,
@@ -10,7 +10,8 @@ import {
   FaFolderOpen,
   FaCalendarAlt,
   FaWpforms,
-} from 'react-icons/fa';
+} from 'react-icons/fa'
+
 export const links = [
   {
     id: 1,
@@ -42,7 +43,7 @@ export const links = [
     text: 'documents',
     icon: <FaWpforms />,
   },
-];
+]
 
 export const social = [
   {
@@ -70,4 +71,4 @@ export const social = [
     url: 'https://www.twitter.com',
     icon: <FaSketch />,
   },
-];
+]
