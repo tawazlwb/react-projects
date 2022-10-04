83aa0ab3615cdf8dbc38517efd48875ab2aef92e
Ismail KHAIRY <tawazlwb@gmail.com>
2022-10-04 18:16:40 +0200

    141. Sidebar/Modal - Context

--- 12-sidebar-modal/setup/src/Home.js
+++ 12-sidebar-modal/setup/src/Home.js
@@ -1,7 +1,11 @@
 import React, { useContext } from 'react'
 import { FaBars } from 'react-icons/fa'
+import { useGlobalContext } from './context'
 
 const Home = () => {
+  const context = useGlobalContext()
+  console.log(context)
+
   return (
     <main>
       <button className='sidebar-toggle'>
