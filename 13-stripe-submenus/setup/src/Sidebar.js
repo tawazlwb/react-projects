f2860c8c39a23293687efb043eb4e49dddd7fea4
Ismail KHAIRY <tawazlwb@gmail.com>
2022-10-05 09:06:47 +0200

    147. Stripe - Sidebar

--- 13-stripe-submenus/setup/src/Sidebar.js
+++ 13-stripe-submenus/setup/src/Sidebar.js
@@ -1,9 +1,38 @@
 import React from 'react'
 import { FaTimes } from 'react-icons/fa'
 import sublinks from './data'
+import { useGlobalContext } from './context'
 
 const Sidebar = () => {
-  return <h2>sidebar component</h2>
+  const { isSidebarOpen, closeSidebar } = useGlobalContext()
+
+  return (
+    <aside className={`sidebar-wrapper ${isSidebarOpen && 'show'}`}>
+      <div className='sidebar'>
+        <button className='close-btn' onClick={closeSidebar}>
+          <FaTimes />
+        </button>
+        {sublinks.map((sublink, index) => {
+          const { page, links } = sublink
+          return (
+            <article key={index}>
+              <h4>{page}</h4>
+              <div className='sidebar-sublinks'>
+                {links.map((link, linkIndex) => {
+                  const { url, icon, label } = link
+                  return (
+                    <a key={linkIndex} href={url}>
+                      {icon} {label}
+                    </a>
+                  )
+                })}
+              </div>
+            </article>
+          )
+        })}
+      </div>
+    </aside>
+  )
 }
 
 export default Sidebar
--- 13-stripe-submenus/setup/src/context.js
+++ 13-stripe-submenus/setup/src/context.js
@@ -4,7 +4,7 @@ import sublinks from './data'
 export const Appcontext = React.createContext()
 
 export const AppPorvider = ({ children }) => {
-  const [isSidebarOpen, setIsSidebarOpen] = useState(true)
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false)
   const [isSubmenuOpen, setIsSubmenuOpen] = useState(true)
 
   const openSidebar = () => {
