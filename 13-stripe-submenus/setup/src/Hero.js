b2a0c891763262f8f4d27a7ceaffe149a7d28680
Ismail KHAIRY <tawazlwb@gmail.com>
2022-10-05 08:55:03 +0200

    146. Stripe - Hero

--- 13-stripe-submenus/setup/src/Hero.js
+++ 13-stripe-submenus/setup/src/Hero.js
@@ -3,10 +3,27 @@ import phoneImg from './images/phone.svg'
 import { useGlobalContext } from './context'
 
 const Hero = () => {
-  const context = useGlobalContext()
-  console.log(context)
+  const { closeSubmenu } = useGlobalContext()
 
-  return <h2>hero component</h2>
+  return (
+    <section className='hero'>
+      <div className='hero-center'>
+        <article className='hero-info'>
+          <h1>payments infrastructure for the internet</h1>
+          <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+            Reprehenderit facere soluta ea autem deserunt corrupti esse
+            eligendi! Quidem aliquam ea itaque quos, alias tempore eligendi
+            aperiam soluta velit facilis cumque.
+          </p>
+          <button className='btn'>Start now</button>
+        </article>
+        <article className='hero-images'>
+          <img src={phoneImg} alt='phone' className='phone-img' />
+        </article>
+      </div>
+    </section>
+  )
 }
 
 export default Hero
