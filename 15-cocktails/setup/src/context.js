64605f654e05d9e9a8f7904b21755922d5444f5c
Ismail KHAIRY <tawazlwb@gmail.com>
2022-10-22 14:12:37 +0200

    193. Cocktails - Context API Setup

--- 15-cocktails/setup/src/context.js
+++ 15-cocktails/setup/src/context.js
@@ -5,8 +5,17 @@ const url = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s='
 const AppContext = React.createContext()
 
 const AppProvider = ({ children }) => {
-  return <AppContext.Provider value='hello'>{children}</AppContext.Provider>
+  const [loading, setLoading] = useState(true)
+  const [searchTerm, setSearchTerm] = useState('a')
+  const [cocktails, setCocktails] = useState([])
+
+  const initialState = { loading, cocktails, setSearchTerm }
+
+  return (
+    <AppContext.Provider value={initialState}>{children}</AppContext.Provider>
+  )
 }
+
 // make sure use
 export const useGlobalContext = () => {
   return useContext(AppContext)
